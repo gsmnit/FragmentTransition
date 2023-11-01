33c10d03936570186dadd9e783ecd0af11465cae
ganpatsingh <2019uee1399@mnit.ac.in>
2023-11-01 14:59:48 +0530

    commiting to save current changes, will try to create simple app first,for complex app i will need these changes, so saving here

--- app/src/main/java/com/example/fragmenttransition/MainActivity.kt
+++ app/src/main/java/com/example/fragmenttransition/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.example.fragmenttransition
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import com.example.fragmenttransition.fragment.GridFragment
 
+/**
+ * Grid to pager app's main activity.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState != null) {
+            currentPosition =
+                savedInstanceState.getInt(KEY_CURRENT_POSITION, 0)
+            // Return here to prevent adding additional GridFragments when changing orientation.
+            return
+        }
+        val fragmentManager: FragmentManager = supportFragmentManager
+        fragmentManager
+            .beginTransaction()
+            .add(R.id.fragment_container, GridFragment(), GridFragment::class.java.simpleName)
+            .commit()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(
+            KEY_CURRENT_POSITION,
+            currentPosition
+        )
+    }
+
+
+    companion object {
+        /**
+         * Holds the current image position to be shared between the grid and the pager fragments. This
+         * position updated when a grid item is clicked, or when paging the pager.
+         *
+         * In this demo app, the position always points to an image index at the [ @link com.example.fragmenttransition.adapter.imageData] class.
+         */
+        @JvmStatic
+        var currentPosition = 0
+        private const val KEY_CURRENT_POSITION =
+            "com.example.fragmenttransition.key.currentPosition"
     }
 }
